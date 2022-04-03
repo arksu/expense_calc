e844d3438e61bb126de3eb8f4cf7093e5d546553
arksu <ark@ark.su>
2022-04-04 00:00:06 +0500

    styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
+// import styles from "./NavBar.scss"
 
 export default function NavBar() {
     return (
-        <>
+        // <nav className={styles.navbar}>
+        <nav>
             <Link href={"/expenses"}><a>Expenses</a></Link>
             <Link href={"/about"}><a>About</a></Link>
-        </>
+        </nav>
     )
 }
---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import {AppProps} from "next/app";
+
+require("@/styles/global.scss")
+
+export default function App({Component, pageProps } : AppProps) {
+    return (
+        <>
+            <Head><title>Expense</title></Head>
+            <main>
+                <Component {...pageProps}></Component>
+            </main>
+        </>
+
+    )
+}
